d8a7de1c9971a32c43965b9f80f641575402ecba
Vadim Markovtsev <vadim@poolside.ai>
2023-10-12 23:03:57 +0200

    Allocate tensors on the input device in FusedLayerNorm

--- xformers/triton/layer_norm.py
+++ xformers/triton/layer_norm.py
@@ -43,8 +43,8 @@ class _LayerNorm(torch.autograd.Function):
         M, N = x_arg.shape
 
         # allocate mean and std, they'll be used in the backward pass
-        mean = torch.empty((M,), dtype=torch.float32, device="cuda")
-        rstd = torch.empty((M,), dtype=torch.float32, device="cuda")
+        mean = torch.empty((M,), dtype=torch.float32, device=x.device)
+        rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
 
         # Less than 64KB per feature: enqueue fused kernel
         MAX_FUSED_SIZE = 65536 // x.element_size()
@@ -114,7 +114,7 @@ class _LayerNorm(torch.autograd.Function):
             GROUP_SIZE_M = GROUP_SIZE_M // 2
 
         # allocate output
-        locks = torch.zeros(2 * GROUP_SIZE_M, dtype=torch.int32, device="cuda")
+        locks = torch.zeros(2 * GROUP_SIZE_M, dtype=torch.int32, device=x.device)
         t_args = {"dtype": x.dtype, "device": x.device}
         _dw = torch.zeros((GROUP_SIZE_M, x.size(-1)), **t_args)
         _db = torch.zeros_like(_dw)
